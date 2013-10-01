0bc742e5edd8d8f0538f12471daba5a8903c9cf2
JayLi <86943843@qq.com>
2013-09-30 22:22:50 -0700

    Add LGPL license, and test switch repo

--- InterviewCodes/InterviewCodes/reverse.cpp
+++ InterviewCodes/InterviewCodes/reverse.cpp
@@ -1,3 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 JayLi
+** All rights reserved.
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
+// Amend 1
+// Amend 2
 
 // Candidate 1
 void rotateM(int *matrix, int n)
